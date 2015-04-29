f93637774258401c9c36d227a89c3fede308d523
matan <dev.matan@gmail.com>
2015-04-29 17:38:42 +0300

    "insufficient cleanup"

--- grapher/src/main/scala/grapher.scala
+++ grapher/src/main/scala/grapher.scala
@@ -4,14 +4,19 @@ import scala.annotation.StaticAnnotation
 import scala.language.experimental.macros
 import scala.reflect.macros.whitebox
 
+class NN extends StaticAnnotation {
+  def macroTransform(annottees: Any*) : Any = macro analyze.impl
+}
+
 object analyze {
   
   def impl(c: whitebox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
     //import Flag._
+    import Console._
     
    @deprecated("","") def extractCaseClassesParts(classDecl: ClassDef) = {
-     println(Console.BLUE_B + "in extractCaseClassesParts" + Console.RESET)
+     println(BLUE_B + "in extractCaseClassesParts" + RESET)
        classDecl match {
          case q"$mods class $className(..$fields) extends ..$parents { ..$body }" =>
            (className, fields, parents, body)
@@ -19,12 +24,12 @@ object analyze {
     }
     
    @deprecated("","") def modifiedDeclaration(classDecl: ClassDef) = {
-      println(Console.BLUE_B + "in modified declaration" + Console.RESET)
+      println(BLUE_B + "in modified declaration" + RESET)
       val (className, fields, parents, body) = extractCaseClassesParts(classDecl)
 
-      //println(Console.BLUE_B + s"before returning (fields are $params)" + Console.RESET)
+      //println(BLUE_B + s"before returning (fields are $params)" + RESET)
       val params = fields.asInstanceOf[List[ValDef]] map { p => p.duplicate}
-      println(Console.BLUE_B + s"before returning" + Console.RESET)
+      println(BLUE_B + s"before returning" + RESET)
       
       c.Expr[Any](
         q"""
@@ -36,37 +41,37 @@ object analyze {
     }
     
     if (annottees.length != 1) { // I have yet to have seen this happen or know what syntax of annotation would allow that
-      print(Console.YELLOW)
+      print(YELLOW)
       println("annottees length: " + annottees.length)
       println("annottes: " + annottees)
-      print(Console.RESET)
+      print(RESET)
     }
     
-    val returnIdentity = c.Expr[Any](Block(annottees.map(_.tree).toList, Literal(Constant(()))))
+    val returnIdentity = c.Expr[Any](Block(annottees.map(_.tree).toList, Literal(Constant(())))) // return expression that duplicates the original annottees (from https://github.com/scalamacros/paradise/blob/5e5f0c129dd1861f86250d7ce94635b89996938c/tests/src/main/scala/identity.scala#L8)
     
     def findMethods(typ: String, name: Any, body: List[c.Tree]) = {
       body map {
         case q"$mods def $tname[..$tparams](...$paramss): $tpt = $expr" =>
-          println(Console.CYAN_B + s"$typ $name" + Console.RESET + 
-                  " has method " + Console.BLUE + Console.BOLD + 
-                  tname + Console.RESET)
+          println(CYAN_B + s"$typ $name" + RESET + 
+                  " has method " + BLUE + BOLD + 
+                  tname + RESET)
           
           expr.foreach { 
             // TODO: need to further hone the quasiquote for capturing only and all cases of method application, *along* the object they are applied to.
-            case q"$obj $f" => println(s"  which calls " + Console.BLUE + Console.BOLD + f + Console.RESET + 
+            case q"$obj $f" => println(s"  which calls " + BLUE + BOLD + f + RESET + 
                                         " on object " + obj + 
-                                        " of type " + Console.CYAN_B + obj.tpe.typeSymbol + Console.RESET)
-                                        // if (obj.tpe != null) println(Console.CYAN_B + obj.tpe.typeSymbol + Console.RESET)
-            case x => // println(Console.YELLOW + s"in unmatched AST part: $x" + Console.RESET); 
+                                        " of type " + CYAN_B + obj.tpe.typeSymbol + RESET)
+                                        // if (obj.tpe != null) println(CYAN_B + obj.tpe.typeSymbol + RESET)
+            case x => // println(YELLOW + s"in unmatched AST part: $x" + RESET); 
             //case q"$obj $f($args)" => println(s"  which calls $f with args $args")
           } 
-        case x => // println(Console.YELLOW + s"in unmatched AST part: $x" + Console.RESET);  
+        case x => // println(YELLOW + s"in unmatched AST part: $x" + RESET);  
       }     
     }
     
-    println(Console.GREEN + "about to typecheck" + Console.RESET)
-    val typeCheckedAnnottees = annottees.map(annottee => c.typecheck(annottee.tree, silent = false)).toList // this doesn't throw, but it will cause the macro to crash upon its return!
-    println(Console.GREEN_B + "typechecking didn't crash!" + Console.RESET)
+    println(GREEN + "about to typecheck" + RESET)
+    val typeCheckedAnnottees = annottees.map(annottee => c.typecheck(annottee.tree, silent = false)).toList // this doesn't throw, but it will cause compilation to abort after all annotations were handled
+    println(GREEN_B + "typechecking didn't crash!" + RESET)
    
     //annottees.map(_.tree).toList match {
     typeCheckedAnnottees match {
@@ -89,17 +94,11 @@ object analyze {
         returnIdentity
         
       case x => 
-        println(Console.YELLOW + "in unmatched AST part" + Console.RESET); q"..$x"
+        println(YELLOW + "in unmatched AST part" + RESET); q"..$x"
         returnIdentity
     }
     
-    //c.Expr[Any](Block(annottees.map(_.tree).toList, Literal(Constant(())))) // return expression that duplicates the original annottees (from https://github.com/scalamacros/paradise/blob/5e5f0c129dd1861f86250d7ce94635b89996938c/tests/src/main/scala/identity.scala#L8)
-   
     //annottees.head
     //c.Expr[Any](result)
   }
-}
-
-class NN extends StaticAnnotation {
-  def macroTransform(annottees: Any*) : Any = macro analyze.impl
 }
